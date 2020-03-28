8387123294af1b740c500a5ff95eec31ed851102
hamidhadi <hamidhadi@live.com>
2020-03-28 14:14:52 +0000

    Add ability to specify position of the button
    - Convert all methods to arrow function
    - Add renderButton method

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-import React, { Component } from "react";
+import React, {Component} from 'react';
 import {
   View,
   TouchableOpacity,
   Platform,
   UIManager,
-  LayoutAnimation
-} from "react-native";
+  LayoutAnimation,
+} from 'react-native';
 
 export default class CollapsibleList extends Component {
   constructor(props) {
     super(props);
 
     if (
-      Platform.OS === "android" &&
+      Platform.OS === 'android' &&
       UIManager.setLayoutAnimationEnabledExperimental
     ) {
       UIManager.setLayoutAnimationEnabledExperimental(true);
@@ -21,31 +21,31 @@ export default class CollapsibleList extends Component {
     this.animationConfig = {
       duration: 700,
       update: {
-        type: "spring",
+        type: 'spring',
         springDamping: 0.7,
-        property: "scaleXY"
-      }
+        property: 'scaleXY',
+      },
     };
 
     this.state = {
       minHeight: 0,
       currentHeight: null,
       collapsed: false,
-      initialized: false
+      initialized: false,
     };
   }
 
-  setMinHeight(event) {
-    const { height: minHeight } = event.nativeEvent.layout;
+  setMinHeight = (event) => {
+    const {height: minHeight} = event.nativeEvent.layout;
 
-    this.setState({ minHeight }, () => {
-      this.setState({ initialized: true, currentHeight: minHeight });
+    this.setState({minHeight}, () => {
+      this.setState({initialized: true, currentHeight: minHeight});
     });
   }
 
-  toggle() {
-    const { minHeight, collapsed } = this.state;
-    const { onToggle, animationConfig } = this.props;
+  toggle = () => {
+    const {minHeight, collapsed} = this.state;
+    const {onToggle, animationConfig} = this.props;
     let nextHeight;
 
     if (collapsed) {
@@ -56,31 +56,47 @@ export default class CollapsibleList extends Component {
 
     LayoutAnimation.configureNext({
       ...this.animationConfig,
-      ...animationConfig
+      ...animationConfig,
     });
-    this.setState({ currentHeight: nextHeight, collapsed: !collapsed }, () => {
+    this.setState({currentHeight: nextHeight, collapsed: !collapsed}, () => {
       if (onToggle) {
         onToggle(this.state.collapsed);
       }
     });
   }
 
-  render() {
-    const { initialized, currentHeight } = this.state;
+  renderButton = () => {
     const {
       numberOfVisibleItems,
       buttonContent,
-      wrapperStyle,
       children
+    } = this.props
+
+    if (numberOfVisibleItems > React.Children.count(children)) return null
+  
+    return (
+      <View>
+        <TouchableOpacity onPress={this.toggle} activeOpacity={0.8}>
+          {buttonContent}
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
+  render() {
+    const {initialized, currentHeight} = this.state;
+    const {
+      numberOfVisibleItems,
+      buttonPosition = 'bottom',
+      wrapperStyle,
+      children,
     } = this.props;
 
     return (
       <View style={wrapperStyle}>
-        <View style={{ overflow: "hidden", height: currentHeight }}>
-          <View
-            style={{ flex: 1 }}
-            onLayout={event => this.setMinHeight(event)}
-          >
+        {buttonPosition === 'top' && this.renderButton()}
+        <View style={{overflow: 'hidden', height: currentHeight}}>
+          <View style={{flex: 1}} onLayout={this.setMinHeight}>
             {React.Children.toArray(children).slice(0, numberOfVisibleItems)}
           </View>
           {initialized && (
@@ -93,13 +109,7 @@ export default class CollapsibleList extends Component {
             </View>
           )}
         </View>
-        {numberOfVisibleItems < React.Children.count(children) && (
-          <View>
-            <TouchableOpacity onPress={() => this.toggle()} activeOpacity={0.8}>
-              {buttonContent}
-            </TouchableOpacity>
-          </View>
-        )}
+        {buttonPosition === 'bottom' && this.renderButton()}
       </View>
     );
   }
